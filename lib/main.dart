1445d92ba000a0801e2297950dba5e17b682ab0b
duong755 <ngo.quang.duong.1100@gmail.com>
2021-04-04 14:00:31 +0700

    Install shared_preferences, remove StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const textStyle = TextStyle(
+    fontSize: 18.0,
+  );
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -13,59 +15,19 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.deepOrange,
       ),
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _fonts = TextStyle(
-    fontSize: 16,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('team 10'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("INT3207 1"),
+        ),
+        body: Container(
+          child: Text(
+            "wemap",
+            style: textStyle,
+          ),
+        ),
       ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _fonts,
-      ),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: EdgeInsets.only(top: 8, bottom: 8, left: 0, right: 0),
-      itemBuilder: (context, index) {
-        if (index.isOdd) {
-          return Divider(
-            color: Colors.blue,
-          );
-        }
-
-        final i = index ~/ 2;
-        if (i >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[i]);
-      },
     );
   }
 }
