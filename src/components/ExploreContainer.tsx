a4e4d24c2b68da40d3d2805114f5b93a217f2e8e
duong755 <ngo.quang.duong.1100@gmail.com>
2021-05-07 22:42:02 +0700

    Display map

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,18 @@
-import './ExploreContainer.scss';
+import { useEffect } from "react";
+
+import "./ExploreContainer.scss";
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  useEffect(() => {
+    new window.wemapgl.WeMap({ key: "GqfwrZUEfxbwbnQUhtBMFivEysYIxelQ", container: "mapContainer" });
+  }, []);
+
   return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <div className="container" id="mapContainer">
     </div>
   );
 };
--- src/wemap.d.ts
+++ src/wemap.d.ts
@@ -1,3 +1,73 @@
+interface WeMapOptions {
+  key: string;
+  container: string | HTMLElement;
+  style?: "default" | "bright" | "dark";
+  center?: [number, number];
+  zoom?: number;
+  reverse?: boolean;
+  traffic?: boolean;
+  urlControlLayer?: boolean;
+}
+
+declare class WeMap {
+  public constructor({
+    style = "default",
+    center = [105.8550736, 21.0283243],
+    zoom = 10,
+    reverse = false,
+    traffic = false,
+    urlControlLayer = true,
+  }: WeMapOptions);
+
+  public addControl(control: any, position?: "top-left" | "top-right" | "bottom-left" | "bottom-right"): WeMap;
+  public removeControl(control: any): WeMap;
+  public addImage(
+    id: string,
+    image:
+      | HTMLImageElement
+      | ImageBitmap
+      | ImageData
+      | { width: number; height: number; data: Uint8Array | Uint8ClampedArray }
+  ): void;
+  public loadImage(url: string, callback: (error: Error, image: any) => void): void;
+}
+
+interface WeGeocoderOptions {
+  key: string;
+  engine?: "default" | "pelias";
+  suggesstion?: {
+    min_char: number;
+  };
+  removeDuplicates?: boolean;
+  flyTo?: boolean | "hybrid";
+  url?: string;
+  placeholder?: string;
+  onSubmitOnly?: boolean;
+  customAttribution?: string;
+  params?: {
+    [key: string]: any;
+  };
+  sources?: WeGeocoderSource | WeGeocoderSource[];
+  marker?: boolean | Object;
+  wof?: boolean | Object;
+}
+
+type WeGeocoderSource = "oa" | "osm" | "wof" | "gn";
+
+declare class WeGeocoder {
+  public constructor(options: WeGeocoderOptions);
+}
+
 declare interface Window {
-  wemapgl: any
+  wemapgl: {
+    WeMap: new ({
+      style = "default",
+      center = [105.8550736, 21.0283243],
+      zoom = 10,
+      reverse = false,
+      traffic = false,
+      urlControlLayer = true,
+    }: WeMapOptions) => WeMap;
+    WeGeocoder: new(weGeocoderOptions: WeGeocoderOptions) => WeGeocoder;
+  };
 }
